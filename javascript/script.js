7b33f509996fc0dfddab0e16af76a8219ded018a
Beny <benizhang1@gmail.com>
2023-09-20 19:41:03 +0700

    add oop to script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,3 @@
-// Objek untuk menyimpan data pendaftar
-let pendaftarData = [];
-
-// Fungsi untuk menghitung rata-rata umur dan uang sangu
-function hitungRataRata() {
-    let totalUmur = 0;
-    let totalUangSangu = 0;
-
-    for (let pendaftar of pendaftarData) {
-        totalUmur += pendaftar.umur;
-        totalUangSangu += pendaftar.uangSangu;
-    }
-
-    const rataRataUmur = totalUmur / pendaftarData.length;
-    const rataRataUangSangu = totalUangSangu / pendaftarData.length;
-
-    return { rataRataUmur, rataRataUangSangu };
-}
-
-// Fungsi untuk menampilkan data pendaftar dan resume
-function tampilkanDataPendaftar() {
-    const tableBody = document.getElementById('pendaftar-data');
-    tableBody.innerHTML = '';
-
-    for (let pendaftar of pendaftarData) {
-        const newRow = tableBody.insertRow();
-        newRow.innerHTML = `
-            <td>${pendaftar.nama}</td>
-            <td>${pendaftar.umur}</td>
-            <td>${pendaftar.uangSangu}</td>
-            <td>Resume: ${pendaftar.resume}</td>
-        `;
-    }
-
-    const rataRata = hitungRataRata();
-    const resumeInfo = document.getElementById('resume-info');
-    resumeInfo.textContent = `Rata-rata pendaftar memiliki uang sangu sebesar ${rataRata.rataRataUangSangu} dengan rata-rata umur ${rataRata.rataRataUmur}`;
-}
-
 // Fungsi untuk menampilkan tab aktif
 function openTab(event, tabName) {
     const tabcontent = document.getElementsByClassName('tabcontent');
@@ -52,33 +13,79 @@ function openTab(event, tabName) {
     document.getElementById(tabName).style.display = 'block';
     event.currentTarget.className += ' active';
 }
+class Registrant {
+    constructor(nama, umur, uangSangu) {
+        this.nama = nama;
+        this.umur = umur;
+        this.uangSangu = uangSangu;
+        this.resume = `Rata-rata pendaftar memiliki uang sangu sebesar ${uangSangu} dengan rata-rata umur ${umur}`;
+    }
+}
 
-// Fungsi untuk mengirim data registrasi
+class DataPendaftar {
+    constructor() {
+        this.pendaftarData = [];
+    }
+
+    tambahPendaftar(pendaftar) {
+        this.pendaftarData.push(pendaftar);
+    }
+
+    hitungRataRata() {
+        let totalUmur = 0;
+        let totalUangSangu = 0;
+
+        for (let pendaftar of this.pendaftarData) {
+            totalUmur += pendaftar.umur;
+            totalUangSangu += pendaftar.uangSangu;
+        }
+
+        const rataRataUmur = totalUmur / this.pendaftarData.length;
+        const rataRataUangSangu = totalUangSangu / this.pendaftarData.length;
+
+        return { rataRataUmur, rataRataUangSangu };
+    }
+}
+
+const dataPendaftar = new DataPendaftar();
+
 function submitForm() {
     const nama = document.getElementById('nama').value;
     const umur = parseInt(document.getElementById('umur').value);
     const uangSangu = parseInt(document.getElementById('uang-sangu').value);
 
-    // Validasi data
     if (nama.length < 10 || umur < 25 || uangSangu < 100000 || uangSangu > 1000000) {
         alert('Data tidak valid. Pastikan Nama minimal 10 karakter, Umur minimal 25 tahun, Uang sangu Rp. 100,000 - Rp. 1,000,000.');
         return;
     }
 
-    // Buat resume
-    const resume = `Rata-rata pendaftar memiliki uang sangu sebesar ${uangSangu} dengan rata-rata umur ${umur}`;
+    const pendaftar = new Registrant(nama, umur, uangSangu);
+    dataPendaftar.tambahPendaftar(pendaftar);
 
-    // Tambahkan data ke pendaftarData
-    pendaftarData.push({ nama, umur, uangSangu, resume });
-
-    // Bersihkan form
     document.getElementById('nama').value = '';
     document.getElementById('umur').value = '';
     document.getElementById('uang-sangu').value = '';
 
-    // Tampilkan data pendaftar
     tampilkanDataPendaftar();
 }
 
-// Inisialisasi halaman
+function tampilkanDataPendaftar() {
+    const tableBody = document.getElementById('pendaftar-data');
+    tableBody.innerHTML = '';
+
+    for (let pendaftar of dataPendaftar.pendaftarData) {
+        const newRow = tableBody.insertRow();
+        newRow.innerHTML = `
+            <td>${pendaftar.nama}</td>
+            <td>${pendaftar.umur}</td>
+            <td>${pendaftar.uangSangu}</td>
+            <td>Resume: ${pendaftar.resume}</td>
+        `;
+    }
+
+    const rataRata = dataPendaftar.hitungRataRata();
+    const resumeInfo = document.getElementById('resume-info');
+    resumeInfo.textContent = `Rata-rata pendaftar memiliki uang sangu sebesar ${rataRata.rataRataUangSangu} dengan rata-rata umur ${rataRata.rataRataUmur}`;
+}
+
 openTab(event, 'registrasi');
